4523aca35ce6c021e80336ca135eda133cf17561
Ward Schodts <schodtsward@gmail.com>
2023-11-10 07:41:48 +0100

    Handle duplicates in the API

--- fluvius_energy_service_company/data_objects/FluviusElectricityMeter.py
+++ fluvius_energy_service_company/data_objects/FluviusElectricityMeter.py
@@ -1,4 +1,5 @@
 from typing import Any
+import logging
 
 from fluvius_energy_service_company.data_objects.FluviusQuarterHourlyEnergy import FluviusQuarterHourlyEnergy
 
@@ -12,7 +13,21 @@ class FluviusElectricityMeter:
         self.seq_number = seq_number
         self.meter_id = meter_id
         self.daily_energy = daily_energy
-        self.quarter_hourly_energy = quarter_hourly_energy
+        self.quarter_hourly_energy = self.set_quarter_hourly_energy(quarter_hourly_energy)
+
+    @staticmethod
+    def set_quarter_hourly_energy(quarter_hourly_energy: list[FluviusQuarterHourlyEnergy]):
+        # Validate wether the quarter_hourly_energy list does not contain any duplicates
+        # if it does, log a warning
+        # but do not return the duplicate values
+        timestamps = []
+        for quarter_hourly in quarter_hourly_energy:
+            if quarter_hourly.timestamp_start in timestamps:
+                logging.warning(f"Duplicate timestamp_start found: {quarter_hourly.timestamp_start}")
+            else:
+                timestamps.append(quarter_hourly.timestamp_start)
+
+        return list(set(quarter_hourly_energy))
 
     def __str__(self):
         energy_str = ''
--- fluvius_energy_service_company/data_objects/FluviusMeasurement.py
+++ fluvius_energy_service_company/data_objects/FluviusMeasurement.py
@@ -10,6 +10,18 @@ class FluviusMeasurement:
         self.injection_value = injection_value
         self.injection_validation_state = injection_validation_state
 
+    def __eq__(self, other):
+        if not isinstance(other, FluviusMeasurement):
+            return NotImplemented
+        return (self.unit, self.offtake_value, self.offtake_validation_state,
+                self.injection_value, self.injection_validation_state) == \
+               (other.unit, other.offtake_value, other.offtake_validation_state,
+                other.injection_value, other.injection_validation_state)
+
+    def __hash__(self):
+        return hash((self.unit, self.offtake_value, self.offtake_validation_state,
+                     self.injection_value, self.injection_validation_state))
+
     def get_offtake_array(self):
         return [self.offtake_value]
 
